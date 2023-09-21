537b03ab13da74b8d8dd455ca022348505fbcdb4
dev.rary@gmail.com <dev.rary@gmail.com>
2023-09-21 11:16:03 +0900

    자동 종료 및 재시작

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello jenkins deploy!!!";
+        return "hello jenkins deploy!!! kill and start";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
