d3dfc383e78e984d1488b10df215ab07ae4f7caf
dev.rary@gmail.com <dev.rary@gmail.com>
2023-09-21 10:56:38 +0900

    롤백

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello jenkins deploy!!!!!!! nohup!!";
+        return "hello";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
