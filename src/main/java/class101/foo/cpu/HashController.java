0b00f0bd7aac030c52edd4d7c97096ecd102aa90
dev.rary@gmail.com <dev.rary@gmail.com>
2023-09-21 11:20:20 +0900

    nohup메세지 추가

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello jenkins deploy!!! kill and start";
+        return "hello jenkins deploy!!! kill and start!!! nohup";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
