452599fedc8d6e0689763b940e6d0b2973625bbc
dev.rary@gmail.com <dev.rary@gmail.com>
2023-09-22 11:52:16 +0900

    도커 기반 자동 배포 테스트

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello jenkins deploy!!! kill and start!!! nohup & log ~ rolling deploy docker based deploy!!!";
+        return "hello ci/cd test";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
